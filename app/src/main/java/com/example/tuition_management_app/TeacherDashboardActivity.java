b2dc36cfb1996f7945e4206b486e564fae3ba3f5
Dilitha Sonal <107531144+dilitha21@users.noreply.github.com>
2025-07-08 12:57:11 +0530

    activity_teacher_ui

--- app/src/main/java/com/example/tuition_management_app/TeacherDashboardActivity.java
+++ app/src/main/java/com/example/tuition_management_app/TeacherDashboardActivity.java
@@ -6,23 +6,29 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TeacherDashboardActivity extends AppCompatActivity {
-    Button btnAttendance, btnAssignment, btnMaterial, btnResult, btnLogout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_dashboard);
 
-        btnAttendance = findViewById(R.id.btnAttendance);
-        btnAssignment = findViewById(R.id.btnAssignment);
-        btnMaterial = findViewById(R.id.btnMaterial);
-        btnResult = findViewById(R.id.btnResult);
-        btnLogout = findViewById(R.id.btnLogout);
+        Button btnAttendance = findViewById(R.id.btnAttendance);
+        Button btnAssignment = findViewById(R.id.btnAssignment);
+        Button btnMaterial = findViewById(R.id.btnMaterial);
+        Button btnResult = findViewById(R.id.btnResult);
+        Button btnLogout = findViewById(R.id.btnLogout);
 
-        btnAttendance.setOnClickListener(v -> startActivity(new Intent(this, AttendanceActivity.class)));
-        btnAssignment.setOnClickListener(v -> startActivity(new Intent(this, UploadAssignmentActivity.class)));
-        btnMaterial.setOnClickListener(v -> startActivity(new Intent(this, UploadMaterialActivity.class)));
-        btnResult.setOnClickListener(v -> startActivity(new Intent(this, UploadResultActivity.class)));
+        btnAttendance.setOnClickListener(v ->
+                startActivity(new Intent(TeacherDashboardActivity.this, AttendanceActivity.class))
+        );
+        btnAssignment.setOnClickListener(v ->
+                startActivity(new Intent(TeacherDashboardActivity.this, UploadAssignmentActivity.class))
+        );
+        btnMaterial.setOnClickListener(v ->
+                startActivity(new Intent(TeacherDashboardActivity.this, UploadMaterialActivity.class))
+        );
+        btnResult.setOnClickListener(v ->
+                startActivity(new Intent(TeacherDashboardActivity.this, UploadResultActivity.class))
+        );
         btnLogout.setOnClickListener(v -> {
             Intent intent = new Intent(this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
